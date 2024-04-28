2f31d9f13281cb4494cae6eb3f57d25202a44d95
Markus Weiss <maweiss@c4b12c5.42berlin.de>
2024-04-28 19:59:01 +0200

    almost there

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 15:22:19 by maweiss           #+#    #+#             */
-/*   Updated: 2024/04/28 15:35:27 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/04/28 19:56:51 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -251,8 +251,8 @@ void ft_size_map(t_vars *vars)
 	}
 	else
 	{
-		vars->map_data->raster_y = (float)vars->map_data->img_height * (680 / (float)vars->map_data->img_height);
-		vars->map_data->raster_x = (float)vars->map_data->img_width *(1240 / (float)vars->map_data->img_width);
+		vars->map_data->raster_y = (680 / (float)vars->map_data->img_height);
+		vars->map_data->raster_x = (1240 / (float)vars->map_data->img_width);
 		if (vars->map_data->raster_y > vars->map_data->raster_x)
 			vars->map_data->raster_y = vars->map_data->raster_x;
 		else
@@ -261,7 +261,209 @@ void ft_size_map(t_vars *vars)
 	ft_recalc_map(vars);
 }
 
-// in this function run through map and put to screen.
+// void	ft_put_bres_90(t_point point1, t_point point2, t_data *img)
+// {
+// 	int	dx;
+// 	int	dy;
+// 	int	delta;
+// 	int	x;
+// 	int	y;
+// 	int xi;
+
+// 	x = 0;
+// 	y = 0;
+// 	delta = 0;
+// 	dx = 0;
+// 	dy = 0;
+// 	xi = 1;
+
+// 	dx = point2.x - point1.x;
+// 	dy = point2.y - point1.y;
+// 	if (dx < 0)
+// 	{
+// 		xi = -1;
+// 		dx = -dx;
+// 	}
+// 	delta = 2 * dx - dy;
+// 	x = point1.x;
+// 	y = point1.y;
+// 	while (y < point2.y)
+// 	{
+// 		if (delta > 0)
+// 		{
+// 			x = x + xi;
+// 			delta = delta + 2 * (dx - dy);
+// 		}
+// 		else
+// 			delta = delta + 2 * dx;
+// 		my_mlx_pixel_put(img, x, y, 0x00FF0000);
+// 		y++;
+// 	}
+// }
+
+void	ft_put_bres_45(t_point point1, t_point point2, t_data *img)
+{
+	int	dx;
+	int	dy;
+	int	delta;
+	int	x;
+	int	y;
+	int yi;
+
+	x = 0;
+	y = 0;
+	delta = 0;
+	dx = 0;
+	dy = 0;
+	yi = 1;
+
+	dx = point2.x - point1.x;
+	dy = point2.y - point1.y;
+	if (dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	delta = (2 * dy) - dx;
+	x = point1.x;
+	y = point1.y;
+	while (x < point2.x)
+	{
+		if (delta > 0)
+		{
+			y = y + yi;
+			delta = delta + (2 * (dy - dx));
+		}
+		else
+			delta = delta + 2 * dy;
+		my_mlx_pixel_put(img, x, y, 0x00FF0000);
+		x++;
+	}
+}
+
+void	ft_put_bres_90(t_point point1, t_point point2, t_data *img)
+{
+	int	dx;
+	int	dy;
+	int	delta;
+	int	x;
+	int	y;
+	int xi;
+
+	x = 0;
+	y = 0;
+	delta = 0;
+	dx = 0;
+	dy = 0;
+	xi = 1;
+
+	dx = point2.x - point1.x;
+	dy = point2.y - point1.y;
+	if (dx < 0)
+	{
+		xi = -1;
+		dx = -dx;
+	}
+	delta = (2 * dx) - dy;
+	x = point1.x;
+	y = point1.y;
+	while (y < point2.y)
+	{
+		if (delta > 0)
+		{
+			x = x + xi;
+			delta = delta + (2 * (dx - dy));
+		}
+		else
+			delta = delta + 2 * dx;
+		my_mlx_pixel_put(img, x, y, 0x00FF0000);
+		y++;
+	}
+}
+
+void	ft_hv_line(t_point act1, t_point act2, t_data *img)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	if (act1.x == act2.x && act1.y < act2.y)
+	{
+		y = act1.y;
+		while (y < act2.y)
+			my_mlx_pixel_put(img, x, y++, 0x00FF0000);
+	}
+	else if (act1.x == act2.x)
+	{
+		y = act2.y;
+		while (y < act1.y)
+			my_mlx_pixel_put(img, x, y++, 0x00FF0000);
+	}
+	else if (act1.y == act2.y && act1.x < act2.x)
+	{
+		x = act1.x;
+		while (x < act2.x)
+			my_mlx_pixel_put(img, x++, y, 0x00FF0000);
+	}
+	else if (act1.y == act2.y)
+	{
+		x = act2.x;
+		while (x < act1.x)
+			my_mlx_pixel_put(img, x++, y, 0x00FF0000);
+	}
+}
+
+void	ft_case_bres(t_point point1, t_point point2, t_data *img, t_vars *vars)
+{
+	t_point	act1;
+	t_point	act2;
+
+	act1.x = point1.x + -vars->map_data->size_x_min + 20;
+	act1.y = point1.y + 20;
+	act2.x = point2.x + -vars->map_data->size_x_min + 20;
+	act2.y = point2.y + 20;
+
+	if (act1.x == act2.x || act1.y == act2.y)
+		ft_hv_line(act1, act2, img);
+	if (act2.y - act1.y < act2.x - act1.x)
+	{
+		if (act1.x > act2.x)
+			ft_put_bres_45(act2, act1, img);
+		else
+			ft_put_bres_45(act1, act2, img);
+	}
+	else
+	{
+		if (act1.x > act2.x)
+			ft_put_bres_45(act2, act1, img);
+		else
+			ft_put_bres_45(act1, act2, img);
+	}
+}
+
+void	ft_bresenham(t_vars *vars, t_data *img)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	x = 0;
+	while (y < vars->map_data->map_height)
+	{
+		x = 0;
+		while (x < vars->map_data->map_width)
+		{
+			if (x + 1 < vars->map_data->map_width)
+				ft_case_bres(vars->map[y][x], vars->map[y][x + 1], img, vars);
+			if (y + 1 < vars->map_data->map_height)
+				ft_case_bres(vars->map[y][x], vars->map[y + 1][x], img, vars);
+			x++;
+		}
+		y++;
+	}
+}
+
 
 void	ft_draw_map(t_vars *vars, t_data *img)
 {
@@ -287,6 +489,7 @@ void	ft_draw_map(t_vars *vars, t_data *img)
 		}
 		y++;
 	}
+	ft_bresenham(vars, img);
 }
 
 
@@ -329,7 +532,7 @@ int	main(int argc, char **argv)
 Todo:
 - add input check;
 - add lines;
-- maybe zoom if small;
+- maybe zoom if small; [x]
 - add Header files and enum;
 - valgrind;
 - error Handling;
